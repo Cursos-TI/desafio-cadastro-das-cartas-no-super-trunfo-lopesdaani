46eed2da0c2d936957e0ba78b138d17492df786c
lopesdaani <lopesdaani@gmail.com>
2024-10-08 15:23:29 +0000

    Criei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 
 int main(){
     printf("Desafio Super Trunfo - Países!\n");
+    printf("Novo commit\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
