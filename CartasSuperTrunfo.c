c76cf7fd15414fb30ab20ce97560fdf5748aad14
lopesdaani <lopesdaani@gmail.com>
2024-10-08 17:21:39 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main(){
+    int populacao, pontos;
+    float area, PIB;
+    char estado[20];
+    char cidade[20];
+    char codigo[20];
+
     printf("Desafio Super Trunfo - Países!\n");
-    printf("Novo commit\n");
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    printf("Digite o estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite a cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área em km²: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &pontos);
+
+    printf("Estado: %s - Nome da cidade: %s\n", estado, cidade);
+    printf("Código da carta: %s - População: %d\n", codigo, populacao);
+    printf("Área em km²: %f - PIB: %f\n", area, PIB);
+    printf("Número de pontos turísticos: %d\n", pontos);
 
     return 0;
 }
